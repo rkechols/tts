1378a7d11b1a6409420b11530f516f34948f89c3
Ryan Echols <rkechols21@gmail.com>
2026-06-08 00:26:19 -0600

    manual text replacements to help pronunciation

--- src/tts/clean_text/main.py
+++ src/tts/clean_text/main.py
@@ -3,13 +3,19 @@ import re
 from argparse import ArgumentParser
 from pathlib import Path
 
+import cmudict
+import yaml
+
 from tts.logging_config import set_logging_config
+from tts.vocab import load_vocab
 
 LOGGER = logging.getLogger("tts.clean_text")
 
+DICTIONARY = set(cmudict.words())
+
 BIONICLE_PREFIXES = ["Av", "Ga", "Ko", "Le", "Onu", "Po", "Ta"]
 
-REPLACEMENTS = {
+REGEX_REPLACEMENTS = {
     re.compile("[‘’]"): "'",  # noqa: RUF001
     re.compile("[“”]"): '"',
     re.compile("[‐—–]"): "-",  # noqa: RUF001
@@ -19,13 +25,42 @@ REPLACEMENTS = {
 }
 
 
-def clean_text(*, text_files_dir_in: Path, text_files_dir_out: Path):
+def _load_replacements_yaml() -> dict[str, str]:
+    with open(Path(__file__).parent / "replacements.yaml", "r", encoding="utf-8") as f:
+        return yaml.safe_load(f)
+
+
+MANUAL_REPLACEMENTS = _load_replacements_yaml()
+
+
+def has_alpha(s: str) -> bool:
+    return any(c.isalpha() for c in s)
+
+
+def clean_text(*, text_files_dir_in: Path, text_files_dir_out: Path, vocab_phonemes_file: Path):
+    vocab_phonemes = load_vocab(vocab_phonemes_file)  # noqa: F841
+
     for file_in in sorted(text_files_dir_in.rglob("*.txt")):
         file_content = file_in.read_text(encoding="utf-8")
         file_content_original = file_content
 
-        for old_re, new in REPLACEMENTS.items():
+        for old_re, new in REGEX_REPLACEMENTS.items():
             file_content = old_re.sub(new, file_content)
+
+        file_content_list: list[str] = []
+        cursor = 0
+        for match in re.finditer(r"\w(?:\w|'(?=\w))*", file_content):
+            file_content_list.append(file_content[cursor : match.start()])
+
+            token = file_content[match.start() : match.end()]
+            token_lower = token.lower()
+
+            if token_lower in MANUAL_REPLACEMENTS:
+                token = MANUAL_REPLACEMENTS[token_lower]
+            file_content_list.append(token)
+            cursor = match.end()
+        file_content_list.append(file_content[cursor:])
+        file_content = "".join(file_content_list)
 
         file_relative = file_in.relative_to(text_files_dir_in)
         file_out = text_files_dir_out / file_relative
@@ -40,8 +75,10 @@ if __name__ == "__main__":
     arg_parser = ArgumentParser(description="Clean text files")
     arg_parser.add_argument("--text-files-dir-in", "-i", type=Path, required=True)
     arg_parser.add_argument("--text-files-dir-out", "-o", type=Path, required=True)
+    arg_parser.add_argument("--vocab", "-v", type=Path, default=None)
     args = arg_parser.parse_args()
     clean_text(
         text_files_dir_in=args.text_files_dir_in,
         text_files_dir_out=args.text_files_dir_out,
+        vocab_phonemes_file=args.vocab,
     )
--- src/tts/kokoro/kokoro_specialized.py
+++ src/tts/kokoro/kokoro_specialized.py
@@ -6,6 +6,8 @@ from typing import Protocol, override
 from kokoro import KPipeline
 from misaki.token import MToken
 
+from tts.vocab import standardize_vocab_token
+
 LOGGER = logging.getLogger("tts.kokoro.kokoro_specialized")
 
 
@@ -15,15 +17,11 @@ class _G2P(Protocol):
 
 
 class _G2PWrapper(_G2P):
-    @staticmethod
-    def standardize_token(s: str) -> str:
-        return s.lower().replace("'", "")
-
     def __init__(self, g2p: _G2P, specialized_vocab: Mapping[str, str]) -> None:
         super().__init__()
         self.g2p = g2p
         self.specialized_vocab = {
-            self.standardize_token(token): phonemes
+            standardize_vocab_token(token): phonemes
             for token, phonemes in specialized_vocab.items()
         }  # fmt: skip
 
@@ -31,7 +29,7 @@ class _G2PWrapper(_G2P):
     def __call__(self, text: str) -> tuple[str, list[MToken]]:
         s, tokens = self.g2p(text)
         for i, token in enumerate(tokens):
-            if known_phonemes := self.specialized_vocab.get(self.standardize_token(token.text)):
+            if known_phonemes := self.specialized_vocab.get(standardize_vocab_token(token.text)):
                 tokens[i] = dataclasses.replace(token, phonemes=known_phonemes)
         return s, tokens
 
