ce7126650b722877a6f8b6f06a9c9ad2c831840c
Ryan Echols <rkechols21@gmail.com>
2026-06-08 04:02:26 -0600

    split out exo prefix

--- src/tts/clean_text/main.py
+++ src/tts/clean_text/main.py
@@ -19,7 +19,7 @@ REGEX_REPLACEMENTS = {
     re.compile("[‐—–]"): "-",  # noqa: RUF001
     re.compile("…"): "...",
     re.compile("[\xa0\xad]"): " ",
-    re.compile(r"\b(av|ga|ko|le|onu|po|ta)-", re.IGNORECASE): r"\1 ",
+    re.compile(r"\b(av|exo|ga|ko|le|onu|po|ta)-", re.IGNORECASE): r"\1 ",
     re.compile(r"\ba+(r?)r*(g?)g*h+\b", re.IGNORECASE): r"a\1\2h",
     re.compile("XV-I"): "XV-1",
 }
